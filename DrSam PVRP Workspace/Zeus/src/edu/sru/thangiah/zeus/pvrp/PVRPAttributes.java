deda6b36ae2c930e9c6bd7d25a4049a6e1c54f65
Josh <programmingeveryday+git@gmail.com>
2014-09-20 23:34:39 -0400

    ff

---
+++ DrSam PVRP Workspace/Zeus/src/edu/sru/thangiah/zeus/pvrp/PVRPAttributes.java
@@ -0,0 +1,9 @@
+package edu.sru.thangiah.zeus.pvrp;
+
+import java.util.jar.Attributes;
+
+/**
+ * Created by joshuasarver on 9/13/2014.
+ */
+public class PVRPAttributes extends Attributes {
+}
---
+++ DrSam PVRP Workspace/Zeus/src/edu/sru/thangiah/zeus/pvrp/pvrpqualityassurance/PVRPQATruckLinkedList.java
@@ -0,0 +1,7 @@
+package edu.sru.thangiah.zeus.pvrp.pvrpqualityassurance;
+
+/**
+ * Created by joshuasarver on 9/13/2014.
+ */
+public class PVRPQATruckLinkedList {
+}
